6fcdcddcccdab15f228384a976463b9a1bbad2a4
sofyanmoch <mchsfyan17@gmail.com>
2020-11-24 09:20:54 +0700

    add automation

--- index.js
+++ index.js
@@ -1,35 +1,53 @@
 // sharp
 
-const sharp = require('sharp')
-const path = require('path')
+const sharp = require("sharp");
+const path = require("path");
+const fs = require("fs");
 
+const compresImg = () => {
+  let destination = "";
+  let dirImg = "./src/assets/source/";
+  const loopDir = () => {
+    let files = [];
+    fs.readdir(dirImg, function (err, file) {
+      if (err) {
+        console.log(err);
+        process.exit(1);
+      }
 
-let destination = ''
-let dirImg = './src/assets/source/'
+      file.forEach((f, index) => {
+        files.push(f);
+      });
+      const getDest = (width, height) => {
+        files.map((e) => {
+          if (width <= 100) {
+            destination = "thumbnail";
+          } else if (width <= 150) {
+            destination = "small";
+          } else if (width <= 400) {
+            destination = "medium";
+          } else if (width <= 1000) {
+            destination = "tablet";
+          } else if (width <= 1900) {
+            destination = "large";
+          }
+          sharp(`./src/assets/source/${e}`)
+            .resize({ width, height })
+            .toFile(
+              `./src/assets/images/${destination}/${destination}-${e}`
+            );
+        });
 
-const getDest = (width, height) => {
-
-    
-
-    if(width <= 100) {
-        destination = 'thumbnail'
-    } else if (width <= 150){
-        destination = 'small'
-    } else if (width <= 400){
-        destination = 'medium'
-    } else if (width <= 1000){
-        destination = 'tablet'
-    } else if(width <= 1900) {
-        destination = 'large'
-    }
-    sharp('./src/assets/source/block-cors.png')
-    .resize({width, height})
-    .toFile(`./src/assets/images/${destination}/halo-${destination}.jpg`
-    )
-}
-
-getDest(100,100)
-getDest(150,150)
-getDest(400,400)
-getDest(1000,1000)
-getDest(1900,1900)
+        //
+      };
+      getDest(100, 100);
+    //   getDest(150, 150);
+    //   getDest(400, 400);
+    //   getDest(1000, 1000);
+    //   getDest(1900, 1900);
+    });
+    return files;
+  };
+  loopDir();
+};
+compresImg();
